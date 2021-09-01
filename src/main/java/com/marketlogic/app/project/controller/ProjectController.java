ae059b673a977d1ec77c264411aac51e225f1d8d
Santhosh Annamalai Sakthivel <santhosh.sakthivel.ext@twago.com>
2021-09-01 17:37:39 +0530

    Updating swagger properties

--- src/main/java/com/marketlogic/app/project/controller/ProjectController.java
+++ src/main/java/com/marketlogic/app/project/controller/ProjectController.java
@@ -27,7 +27,8 @@ public class ProjectController {
 
     @GetMapping
     @ApiOperation(value = "To get the all projects", response = ProjectResponse.class)
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully retrieved all projects")})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully retrieved all projects"),
+            @ApiResponse(code = 500, message = "Please contact the owner")})
     public ProjectResponse getAllProjects(@RequestParam(defaultValue = "1") @ApiParam int page,
                                           @RequestParam(defaultValue = "25") @ApiParam int size) {
         return projectService.findAll(page, size);
@@ -37,7 +38,8 @@ public class ProjectController {
     @ApiOperation(value = "To get a project by Id", response = ProjectDTO.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully retrieved project"),
             @ApiResponse(code = 400, message = "Invalid project id"),
-            @ApiResponse(code = 404, message = "Project not found")})
+            @ApiResponse(code = 404, message = "Project not found"),
+            @ApiResponse(code = 500, message = "Please contact the owner")})
     public ResponseEntity<ProjectDTO> getProjectById(@PathVariable long projectId) {
         validateProjectId(projectId <= 0);
         var project = projectService.findById(projectId);
@@ -51,7 +53,8 @@ public class ProjectController {
     @PostMapping
     @ApiOperation(value = "To create a project", response = ProjectDTO.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully created a project"),
-            @ApiResponse(code = 400, message = "Invalid project details")})
+            @ApiResponse(code = 400, message = "Invalid project details"),
+            @ApiResponse(code = 500, message = "Please contact the owner")})
     public ResponseEntity<ProjectDTO> createProject(@RequestBody ProjectDTO projectDTO) {
         if (projectDTO == null || StringUtils.isBlank(projectDTO.getTitle())) {
             throw new AppServiceException(ErrorCode.BAD_REQUEST);
@@ -63,7 +66,8 @@ public class ProjectController {
     @ApiOperation(value = "To update the project by Id", response = ProjectDTO.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully updated the project"),
             @ApiResponse(code = 400, message = "Invalid project details"),
-            @ApiResponse(code = 404, message = "Project not found")})
+            @ApiResponse(code = 404, message = "Project not found"),
+            @ApiResponse(code = 500, message = "Please contact the owner")})
     public ResponseEntity<ProjectDTO> updateProject(@PathVariable long projectId,
                                                     @RequestBody ProjectDTO projectDTO) {
         validateProjectId(projectId <= 0 || projectDTO == null);
@@ -74,7 +78,8 @@ public class ProjectController {
     @ApiOperation(value = "To delete the project by Id", response = HttpStatus.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully deleted the project"),
             @ApiResponse(code = 400, message = "Invalid project details"),
-            @ApiResponse(code = 404, message = "Project not found")})
+            @ApiResponse(code = 404, message = "Project not found"),
+            @ApiResponse(code = 500, message = "Please contact the owner")})
     public ResponseEntity<HttpStatus> deleteById(@PathVariable long projectId) {
         validateProjectId(projectId <= 0);
         projectService.deleteById(projectId);
@@ -86,7 +91,8 @@ public class ProjectController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully published the project"),
             @ApiResponse(code = 400, message = "Invalid project details"),
             @ApiResponse(code = 404, message = "Project not found"),
-            @ApiResponse(code = 409, message = "Project already published")})
+            @ApiResponse(code = 409, message = "Project already published"),
+            @ApiResponse(code = 500, message = "Please contact the owner")})
     public ResponseEntity<HttpStatus> publishById(@PathVariable long projectId) {
         validateProjectId(projectId <= 0);
         projectService.publishById(projectId);
