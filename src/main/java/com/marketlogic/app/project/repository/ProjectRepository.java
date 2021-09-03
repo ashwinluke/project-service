0bcf43a120bbac227e0ae5bd73c5487fac8c6990
Santhosh Annamalai Sakthivel <santhosh.sakthivel.ext@twago.com>
2021-09-03 18:30:31 +0530

    Improvements

--- src/main/java/com/marketlogic/app/project/repository/ProjectRepository.java
+++ src/main/java/com/marketlogic/app/project/repository/ProjectRepository.java
@@ -3,9 +3,16 @@ package com.marketlogic.app.project.repository;
 import com.marketlogic.app.project.entity.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Integer> {
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = {
+                    "sections"
+            }
+    )
     Page<Project> findAll(Pageable pageable);
 
     Project findById(long id);
