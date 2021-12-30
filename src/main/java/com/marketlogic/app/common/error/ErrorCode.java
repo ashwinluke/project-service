a2da5ba71ee53cf74fbb346c7c0fd2bba885013e
Ashwin Babu <ashwin.babu.ext@siemens-energy.com>
2021-12-30 17:39:26 +0530

    Updated error message

--- src/main/java/com/marketlogic/app/common/error/ErrorCode.java
+++ src/main/java/com/marketlogic/app/common/error/ErrorCode.java
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-    PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "Project not found"),
-    SECTION_NOT_FOUND(HttpStatus.NOT_FOUND, "Section not found"),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Invalid input"),
-    CONFLICTS(HttpStatus.CONFLICT, "Invalid input");
+    PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "Project not found!"),
+    SECTION_NOT_FOUND(HttpStatus.NOT_FOUND, "Section not found!"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Invalid input!"),
+    CONFLICTS(HttpStatus.CONFLICT, "Invalid input!");
 
     private final HttpStatus httpStatus;
     private final String message;
